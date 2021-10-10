c620351b0476a014fb8c9ee2dba1fb49f4e59df6
Eugene Belinski <ebelinski@protonmail.com>
2021-10-10 13:22:31 -0500

    initial commit

---
+++ CouldNotFindSnapKitError/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CouldNotFindSnapKitError
+//
+//  Created by Eugeniu Belinski on 10/10/21.
+//
+
+import UIKit
+import SnapKit
+import Meridian
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
